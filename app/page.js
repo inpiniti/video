6b4f2eb3c6c83127c2debfcceef2283aa971d758
youngkyunjung <younginpiniti@gmail.com>
2025-10-18 20:50:57 +0900

    필요없는 코드 제거

--- app/page.js
+++ app/page.js
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState, useEffect, useRef, useCallback } from "react";
+import { useState, useEffect, useRef } from "react";
 import { useRouter } from "next/navigation";
 import { Plus } from "lucide-react";
 import { Spinner } from "@/components/ui/spinner";
